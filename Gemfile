12fb374cfd1be682fcb4682f7267f29ce721d267
Chris Lawrence <cl@chrislawrence.me>
2013-06-30 09:15:02 +1000

    added deployment config

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.0.rc2'
 gem 'pg'
+gem 'unicorn'
 
 gem 'sass-rails', '~> 4.0.0.rc2'
 gem 'uglifier', '>= 1.3.0'
--- spec/controllers/pomodoros_controller_spec.rb
+++ spec/controllers/pomodoros_controller_spec.rb
@@ -13,7 +13,7 @@ describe PomodorosController do
 
   it "returns default values if user not logged in" do
     get :index, :format => :json
-    expect(response.body).to eq("{\"user\":{\"id\":1,\"work_time\":25,\"break_time\":5}}")
+    expect(response.body).to eq("{\"user\":{\"id\":1,\"work_time\":25,\"break_time\":5,\"long_break_time\":15}}")
   end
 
   it "creates a pomodoro when user logged in" do
