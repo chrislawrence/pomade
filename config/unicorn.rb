83aa3c1d823db26876f06fac3ce7bbbb68235262
Chris Lawrence <cl@chrislawrence.me>
2013-07-04 12:15:46 +1000

    messing with unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,36 @@
-root = "/home/chris/apps/pomade/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+env = ENV["RAILS_ENV"] || "development"
+pid = "/tmp/unicorn.pomade.pid"
+worker_processes 4
 
-listen "/tmp/unicorn.pomade.sock"
-worker_processes 2
-timeout 30
+listen "/tmp/pomade.socket", :backlog => 64
 preload_app true
+timeout 30
+
+working_directory "/home/chris/apps/pomade/current"
+user "chris"
+shared_path = "/home/chris/apps/pomade/shared"
+
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  old_pid = "/tmp/unicorn.pomade.pid.oldbin"
+  
+  if File.exists?(old_pid) && serer.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno:ENOENT, Errno:ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
+
