d98c9cc86c87719c10829a2a21bf8200642a5d16
Chris Lawrence <cl@chrislawrence.me>
2013-11-09 09:17:08 +1100

    First commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'ember-rails'
+gem 'ember-source'
+
+gem 'rspec-rails', :group => [:test, :development]
+group :test do
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem 'rb-fsevent', '~> 0.9'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pomade::Application.config.secret_key_base = 'c08076434bedfbca5cab0c36431bb90f32a96c4d9cd3582183bfa991a412d94fd40656dcb4f219d21d9a62ede7f801937d37ecaa6b81a95f6093d2442cc96250'
